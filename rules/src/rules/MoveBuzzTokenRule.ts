2a688c16ec96868cc193b2e0a154d980502241c9
Patrick Beaume <p.beaume@gmail.com>
2024-10-07 07:49:13 +0200

    undo erasing rfromi updates in movebuzztoien

--- rules/src/rules/MoveBuzzTokenRule.ts
+++ rules/src/rules/MoveBuzzTokenRule.ts
@@ -13,7 +13,22 @@ import { RuleId } from './RuleId'
 
 export class MoveBuzzTokenRule extends BasePlayerTurnRule {
   onRuleStart() {
-    return []
+    const buzz = this.buzz
+    if (buzz !== undefined && buzz !== Buzz.TheKingBuzz) {
+      const buzzItem = this.material(MaterialType.Buzz).id(buzz).getItem<Buzz>()!
+      if (buzzItem.location.type === LocationType.FameTrack || buzzItem.location.type === LocationType.DestructionTrack) {
+        const buzzSpaces = this.getBuzzSpaces(buzzItem.location, buzz)
+        const pawnItem = this.material(MaterialType.Pawn).location(buzzItem.location.type).getItem()!
+        if (buzzSpaces.some(space => space.x === pawnItem.location.x)) {
+          // The fame or destruction marker is on the buzz token, it cannot be moved
+          return this.startNextRule
+        } else {
+          // The buzz token is on the track: move it up a little bit so that player can rotate it and replace it on the original spot or wherever he likes
+          return [this.material(MaterialType.Buzz).id(buzz).moveItem(item => ({ ...item.location, y: -1 }))]
+        }
+      }
+    }
+    return this.getPlayerMoves().length === 0 ? this.startNextRule : []
   }
 
   getPlayerMoves() {
@@ -61,14 +76,16 @@ export class MoveBuzzTokenRule extends BasePlayerTurnRule {
   }
 
   afterItemMove(move: ItemMove) {
-    if (!isMoveItemType(MaterialType.Buzz)(move)) return []
+    return isMoveItemType(MaterialType.Buzz)(move) ? this.startNextRule : []
+  }
 
+  get startNextRule() {
     const effects = new EffectHelper(this.game, this.player).applyEffectMoves()
     if (effects.length) {
       return effects
+    } else {
+      return [this.startRule(RuleId.Buy)]
     }
-
-    return [this.startRule(RuleId.Buy)]
   }
 
   get effects() {
