7c8646ad3342d49792d44a792fe7f53fe716acda
Patrick Beaume <p.beaume@gmail.com>
2024-10-16 12:18:03 +0200

    Spiked tail will increase smash count if multiple pawns occured

--- rules/src/rules/keep/card/SpikedTailKeepRule.ts
+++ rules/src/rules/keep/card/SpikedTailKeepRule.ts
@@ -1,15 +1,41 @@
+import { PowerCard } from '../../../material/cards/PowerCard'
+import { MaterialType } from '../../../material/MaterialType'
 import { Pawn } from '../../../material/Pawn'
 import { EffectType } from '../../effects/EffectType'
+import { EffectWithSource } from '../../effects/EffectWithSource'
+import { Memory } from '../../Memory'
 import { KeepRule } from '../KeepRule'
 
 
 export class SpikedTailKeepRule extends KeepRule {
   afterPullPawn(pawn: Pawn) {
     if (this.getActivePlayer() !== this.cardPlayer || pawn !== Pawn.Destruction) return
-    // TODO: search for existing Spiked tail effect in the memory to increase count instead of creating a new one
-    this.pushEffect({
-      type: EffectType.Smash,
-      count: 1
-    }, this.rival)
+    const spikedTailSmash = this.spikedTailSmash
+    if (spikedTailSmash) {
+      spikedTailSmash.effect.count += 1
+    } else {
+      this.pushEffect({
+        type: EffectType.Smash,
+        count: 1
+      }, this.rival)
+    }
+
+  }
+
+  get effects() {
+    return this.remind<EffectWithSource[]>(Memory.Effects) ?? []
+  }
+
+  get spikeTail() {
+    return this.material(MaterialType.PowerCard)
+      .id(PowerCard.SpikedTail)
+  }
+
+  get spikedTailSmash() {
+    return this.effects
+      .find((e) =>
+        e.effect.type === EffectType.Smash &&
+        e.sources.some((source) => source.type === MaterialType.PowerCard && source.indexes.includes(this.spikeTail.getIndex()))
+      )
   }
 }
