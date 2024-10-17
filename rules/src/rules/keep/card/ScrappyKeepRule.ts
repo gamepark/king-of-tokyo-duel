3407b07e7b37c8d53fb439d35e00636202a3bb02
Romain Fromi <romain.fromi@gmail.com>
2024-10-17 20:35:08 +0200

    Implement ScrappyKeepRule

--- rules/src/rules/keep/card/ScrappyKeepRule.ts
+++ rules/src/rules/keep/card/ScrappyKeepRule.ts
@@ -1,18 +1,29 @@
+import { getBuzzSpaces } from '../../../material/Buzz'
+import { LocationType } from '../../../material/LocationType'
 import { MaterialType } from '../../../material/MaterialType'
+import { Pawn } from '../../../material/Pawn'
 import { EffectType } from '../../effects/EffectType'
 import { KeepRule } from '../KeepRule'
 
-
 export class ScrappyKeepRule extends KeepRule {
-  afterPullPawn() {
+  afterPullPawn(pawn: Pawn) {
     if (this.getActivePlayer() !== this.cardPlayer) return
     if (this.bonusTokensCount === 2) return
+    const location = this.material(MaterialType.Pawn).id(pawn).getItem()!.location
+    const previousX = this.cardPlayer === this.game.players[0] ? location.x! + 1 : location.x! - 1
+    const currentBuzz = this.getBuzzAtX(location.type, location.x!)
+    const previousBuzz = this.getBuzzAtX(location.type, previousX)
+    if (previousBuzz.length === 1 && previousBuzz.getIndex() !== currentBuzz.getIndex()) {
+      this.unshiftEffect({
+        type: EffectType.GetWhiteDiceToken,
+        count: 1
+      }, this.cardPlayer)
+    }
+  }
 
-    // TODO @rfromi: define "each time you pull the pawn marker of a buzz token"
-    this.pushEffect({
-      type: EffectType.GetWhiteDiceToken,
-      count: 1
-    }, this.cardPlayer)
+  getBuzzAtX(track: LocationType, x: number) {
+    return this.material(MaterialType.Buzz).location(track)
+      .filter(item => getBuzzSpaces(item.location, item.id!).some(space => Math.abs(space.x - x) <= 0.5))
   }
 
   get bonusTokensCount() {
